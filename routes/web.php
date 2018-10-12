79397192ef3446a4fa1ff3b1036cb531cff0bc75
Wesley Denaro <Wesley.Denaro@iCloud.com>
2018-10-11 23:00:23 -0400

    Adjusted CSS ad IMG tags to not be root-relative. This will add flexibility to where this app can be installed.

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
 
-//  Default route will deliver a simple splash screen
+// This route will display the details of a random card, pulled from the
+// database. If the optional ID parameter is provided, it will display
+// that particular card's detail
+    Route::get('card/{id?}', 'cardsController@show')
+        ->where('id', '[0-9]+')
+        ->name('card.show');
+
+
+// Default route will deliver a simple splash screen
     Route::get('/', function () {
         return view('splash');
     });
-
-
-/*  This route will display the details of a random card, pulled from the
-    database. If the optional ID parameter is provided, it will display
-    that particular card's detail. */
-    Route::get('/card/{id?}', 'cardsController@show')
-        ->where('id', '[0-9]+')
-        ->name('card.show');
